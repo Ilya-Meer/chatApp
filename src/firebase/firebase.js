e119fdea01a2f06e86a82f3c8bb9efaa6a9b1188
Ilya <ilya.meerov@gmail.com>
2018-10-02 09:58:27 -0700

    Added environment file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyDlxH3Dm9kKQETbuWdtEvy1zDZbl80yk98',
-  authDomain: 'chatapp-459ef.firebaseapp.com',
-  databaseURL: 'https://chatapp-459ef.firebaseio.com',
-  projectId: 'chatapp-459ef',
-  storageBucket: 'chatapp-459ef.appspot.com',
-  messagingSenderId: '957890728205',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
